d073b0241bb308e7f329a5705b4bdd9fc2193791
Saeed <saeed_et90@yahoo.com>
2021-10-17 00:01:30 +1100

    stats added

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 https://job-tracker-tau.vercel.app/
 
 # This is a handy project that I've created to help me keep track of my boring job hunting processes
+
+# TypeScript - React - Local Storage
+
+### Pretty simple but usefull
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Job } from "../types/JobTypes";
 import { giveMeSortedJobs } from "../utils/utils";
 import "./styles/Header.css";
@@ -11,12 +11,20 @@ interface Props {
 
 const Header: React.FC<Props> = ({ setDisplayInput, setJobs, jobs }) => {
   const [sort, setSort] = useState<string>("date");
+  const [moved, setMoved] = useState<number>(0);
+  const [rejected, setRejected] = useState<number>(0);
   const sortHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSort(event.target.value);
     setJobs(giveMeSortedJobs(event.target.value, jobs));
   };
+  useEffect(() => {
+    setMoved(jobs.filter((job: any) => job.status === "interview").length);
+    setRejected(jobs.filter((job: any) => job.status === "rejected").length);
+  }, [jobs]);
+  console.log(jobs);
   return (
     <header className="header">
+      {/* left section */}
       <div className="header__left">
         <div className="header__leftTitle">Sort By</div>
         <div className="header__leftInputs">
@@ -43,11 +51,33 @@ const Header: React.FC<Props> = ({ setDisplayInput, setJobs, jobs }) => {
             />
           </div>
         </div>
+        <div className="header__leftInfo">
+          <p className="app_moved">
+            <span>{moved}</span> Moved
+          </p>
+          <p className="app_waiting">
+            <span>{jobs.length - (rejected + moved)}</span> Waiting
+          </p>
+          <p className="app_rejected">
+            <span>{rejected}</span> Rejected
+          </p>
+        </div>
       </div>
+      {/* middle section */}
       <div className="header__middle">
         <button onClick={() => setDisplayInput(true)}>Add a job</button>
       </div>
+      {/* right section */}
       <div className="header__right">
+        <div>
+          <p className="app_total">
+            <span>{jobs.length}</span>{" "}
+            <span className="app_total_responsive">
+              Application{jobs.length > 1 && "s"} in
+            </span>{" "}
+            Total
+          </p>
+        </div>
         <button onClick={() => setJobs([])}>Clear All</button>
       </div>
     </header>
